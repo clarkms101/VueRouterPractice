87e1938d06f980be8d154eb5b14ff010dffe9aed
clarklin <clarkms101@gmail.com>
2021-06-07 22:28:23 +0800

    add router-Page Childs

--- vue-router-practice/src/router/index.js
+++ vue-router-practice/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Hello from '@/components/HelloWorld';
 import Page from '@/components/pages/page';
+import child from '@/components/pages/child';
+import child2 from '@/components/pages/child2';
+import child3 from '@/components/pages/child3';
 
 Vue.use(VueRouter);
 
@@ -15,7 +18,24 @@ export default new VueRouter({
         {
             name: '分頁',
             path: '/page',
-            component: Page
+            component: Page,
+            children: [
+                {
+                    name: '卡片 1',
+                    path: '', // 不填會變成預設帶入的路徑
+                    component: child
+                },
+                {
+                    name: '卡片 2',
+                    path: 'child2',
+                    component: child2
+                },
+                {
+                    name: '卡片 3',
+                    path: 'child3',
+                    component: child3
+                }
+            ]
         }
     ]
 })
